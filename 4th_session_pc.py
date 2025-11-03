e914765ffcee4be3106c9e19b8c27f53994b598e
AR1ANN8 <125408625+AR1ANN8@users.noreply.github.com>
2025-11-03 21:21:01 +0330

    add from pycharm

---
+++ 4th_session_pc.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import requests
+with st.sidebar:
+    st.write("weather app!")
+    st.image("51ZgFK-FbwL._h1_.png")
+btn = st.button("click on me for get weather information!")
+if btn:
+    lat = str(input("enter your latitude: "))
+    lon = str(input("enter your longitude: "))
+    key = str(input("enter key: "))
+    api = f"https://api.weatherbit.io/v2.0/current?lat={lat}&lon={lon}&key=[-]&include=minutely"
+    response = requests.get(api)
+    st.write(response.status_code)
+    st.write(response.json())
+    
